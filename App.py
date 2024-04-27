10779f81169a5a6948068e0a06d8ae1952e10977
nabe189 <eisuke189@gmail.com>
2024-04-27 19:58:00 +0900

    uploaded image

--- App.py
+++ App.py
@@ -124,7 +124,11 @@ def main():
 
     if uploaded_image is not None:
         # アップロードされた画像を読み込む
-        image = Image.open(uploaded_image)
+        #image = Image.open(uploaded_image)
+        # BytesIOオブジェクトに読み込む
+        image_bytes = uploaded_image.read()
+        # BytesIOオブジェクトを使ってPILのImageを作成
+        image = Image.open(io.BytesIO(image_bytes))
         processed_image = preprocess_image(image, ncols, nrows) #画像の前処理
         outputs = divide_image(processed_image, ncols, nrows, preview=True)
         
