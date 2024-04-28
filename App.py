1e47c098e6247fd1726b456855ecbfce3041419d
nabe189 <eisuke189@gmail.com>
2024-04-28 12:05:23 +0900

    a

--- App.py
+++ App.py
@@ -130,7 +130,8 @@ def main():
         #image_bytes = io.BytesIO(uploaded_image.getvalue()).decode("utf-8").read()
         # BytesIOオブジェクトを使ってPILのImageを作成
         bytes_data = uploaded_image.getvalue()
-        stringio = io.StringIO(bytes_data.decode("utf-8"))
+        #stringio = io.StringIO(bytes_data.decode("utf-8"))
+        stringio = io.BytesIO(bytes_data.decode("utf-8"))
         image = Image.open(stringio)
         processed_image = preprocess_image(image, ncols, nrows) #画像の前処理
         outputs = divide_image(processed_image, ncols, nrows, preview=True)
